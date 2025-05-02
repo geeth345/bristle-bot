a3fd8dbfa12f10158b5714145132aee7519a1088
Maglasch <150831227+Maglasch@users.noreply.github.com>
2025-05-02 11:12:25 +0100

    Update Locomotion.cpp

--- bristle bot/src/Locomotion.cpp
+++ bristle bot/src/Locomotion.cpp
@@ -2,12 +2,12 @@
 #include <Arduino.h>
 
 //motor pins fo Seeed
-static const int motorRight = 0;
-static const int motorLeft = 2;
+static const int motorRight = 2;
+static const int motorLeft = 3;
 
 //lévy walk maximum and minimum times
 static const int minWalkTime = 500;   // Minimum walk interval (ms)
-static const int maxWalkTime = 4000;  // Maximum walk interval (ms)
+static const int maxWalkTime = 2000;  // Maximum walk interval (ms)
 
 //timing markers for the walk 
 static unsigned long previousMillis = 0;
@@ -17,6 +17,7 @@ void initialiseLocomotion() {
   pinMode(motorRight, OUTPUT);
   pinMode(motorLeft, OUTPUT);
 
+  Serial.begin(115200);  //initialise serial port for debugging
   //initialize walk timing to some random interval between min/max, set timer
   interval = random(minWalkTime, maxWalkTime);
   previousMillis = millis();
@@ -28,31 +29,51 @@ void updateLocomotion() {
   if (currentMillis - previousMillis >= interval) { //loop around timer until random interval met.
     previousMillis = currentMillis;
     levyWalk(); //perform levy walk
-    interval = random(minWalkTime, maxWalkTime); //reset interval
   }
 }
 
+//OLD
 //actual locomotion control 
-void levyWalk() {  
-  int action = random(3); //every time interval is exceeded, choose one of the three cases
-  switch (action) {
-    case 0: moveForward(); break;
-    case 1: turnLeft(); break;
-    case 2: turnRight(); break;
+//void levyWalk() {  
+//  int action = random(3); //every time interval is exceeded, choose one of the three cases
+//  switch (action) {
+//    case 0: moveForward(); break;
+//    case 1: turnLeft(); break;
+//    case 2: turnRight(); break;
+//  }
+//}
+
+void levyWalk() {
+  int r = random(100);  //probabilities for each to happen, i.e 60% to go forwards, 20%ea to turn left/right
+
+  if (r < 60) { //biased to forwards walk
+    moveForward();
+    interval = random(minWalkTime, maxWalkTime);
+  }
+  else if (r < 80) { //short turns left+right
+    turnLeft();
+    interval = random(minWalkTime, 1000);
+  }
+  else {
+    turnRight();
+    interval = random(minWalkTime, 1000);
   }
 }
 
 void moveForward() {
+  Serial.write("Moving Forward");
   digitalWrite(motorRight, HIGH);
   digitalWrite(motorLeft, HIGH);
 }
 
 void turnLeft() {
+  Serial.write("Turning Left");
   digitalWrite(motorRight, HIGH);
   digitalWrite(motorLeft, LOW);
 }
 
 void turnRight() {
+  Serial.write("Turning Right");
   digitalWrite(motorRight, LOW);
   digitalWrite(motorLeft, HIGH);
 }
