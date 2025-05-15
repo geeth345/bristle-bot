0b43acb329074e8723d79bf957b11bb3f0ec373d
Daniel Hobbs (dakh1g20) <dakh1g20@soton.ac.uk>
2025-05-15 18:35:46 +0100

    update command server

--- command server/main.py
+++ command server/main.py
@@ -3,11 +3,16 @@ import time
 import yaml
 from pathlib import Path
 import bleak
+from websockets.asyncio.server import serve
+import websockets
+import argparse
+import json
 
 from loguru import logger
 
 bot_disconnect_timeout = 10
 bot_remove_timeout = 60
+webserver_port = 8000
 
 company_ids = {}
 
@@ -17,17 +22,28 @@ class Bot:
         self.id = bot_id
         self.bluetooth_mac = bluetooth_mac
         self.last_seen = time.time()
-        self.status = {"connected" : True, "rssi" : rssi, "manuf_data" : manuf_data}
+        self.status = {"connected" : True, "rssi" : rssi, "manuf_data" : manuf_data[65535]}
         self.name = name
+        raw_data = manuf_data[65535]
+        self.status["manuf_data"] = raw_data
+        self.x_position = raw_data[0]
+        self.y_position = raw_data[1]
+        self.status["Battery_level"] = raw_data[2]
+        self.status["Sound_Level"] = raw_data[3]
     
     def refresh_data(self, rssi, manuf_data):
         self.status["connected"] = True
         self.last_seen = time.time()
         self.status["rssi"] = rssi
-        self.status["manuf_data"] = manuf_data
+        raw_data = manuf_data[65535]
+        self.status["manuf_data"] = raw_data
+        self.x_position = raw_data[0]
+        self.y_position = raw_data[1]
+        self.status["Battery_level"] = raw_data[2]
+        self.status["Sound_Level"] = raw_data[3]
     
     def __str__(self):
-        return f"{{{self.name}  id:{{{self.id}}} rssi: {{{self.status["rssi"]}}} manuf_name: {{{self.status["manuf_data"]}}}}}"
+        return f"{{{self.name}  id:{{{self.id}}} rssi: {{{self.status["rssi"]}}} manuf_data: {{{bytes.hex(self.status["manuf_data"])}}}}}"
 
 bot_registry: dict[str, Bot] = {}
 
@@ -88,11 +104,11 @@ async def timeout_loop():
             else:
                 count += 1
         logger.info("Known connected devices: {}/{}", count, len(bot_registry))
-        # for bot in bot_registry.values():
-        #     if bot.status["connected"]:
-        #         logger.info("Device: {}", bot)
-        #     else:
-        #         logger.warning("Device: {}", bot)
+        for bot in bot_registry.values():
+            if bot.status["connected"]:
+                logger.info("id: {} last seen: {} battery: {} x: {} y: {} sound: {}", bot.id, bot.last_seen, bot.status["Battery_level"], bot.x_position, bot.y_position, bot.status["Sound_Level"])
+            else:
+                logger.warning("id: {} last seen: {} battery: {} x: {} y: {} sound: {}", bot.id, bot.last_seen, bot.status["Battery_level"], bot.x_position, bot.y_position, bot.status["Sound_Level"])
         await asyncio.sleep(1)
 
 async def cleanup():
@@ -102,19 +118,50 @@ async def cleanup():
         delete_queue = []
         for bot in bot_registry.values():
             if (bot.last_seen + bot_remove_timeout < now):
-                #logger.warning(f"Bot {{{bot.id}}} not seen for more than {bot_remove_timeout} seconds.")
+                logger.warning(f"Bot {{{bot.id}}} not seen for more than {bot_remove_timeout} seconds.")
                 delete_queue.append(bot.id)
         for to_delete in delete_queue:
             bot_registry.pop(to_delete)
         await asyncio.sleep(1)
 
+async def get_data(websocket):
+    logger.info("Got connection to server!")
+    try:
+        while True:
+            #do something
+            logger.debug("Gathering data")
+            # Generate JSOn data
+            json_output = f"{{ \"timestamp\": {time.time()}, \"robots\": ["
+            for bot in bot_registry.values():
+                json_output += f"{{\"id\": \"{bot.id}\","
+                json_output += f"\"position\": {{\"x\": {bot.x_position}, \"y\": {bot.y_position}}},"
+                json_output += "},"
+            json_output += "]}"
+            await websocket.send(json_output)
+            await asyncio.sleep(1)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+
+async def manage_webserver():
+    logger.info("Starting server on port {}", webserver_port)
+    async with serve(get_data, "localhost", webserver_port) as server:
+        await server.serve_forever()
+
 async def main():
-    logger.debug("Reading manufacturer IDs")
+    # logger.debug("Reading manufacturer IDs")
     indata = yaml.safe_load(Path("company_identifiers.yaml").read_text())["company_identifiers"]
     for d in indata:
         company_ids[d["value"]] = d["name"]
     #logger.debug(company_ids)
-    await asyncio.gather(scan_loop(), timeout_loop(), cleanup())
+    try:
+        await asyncio.gather(scan_loop(), timeout_loop(), cleanup(), manage_webserver())
+    except asyncio.exceptions.CancelledError:
+        logger.error("Cancelled, now exiting...")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Command Server")
+    parser.add_argument("--port", help="Which port to run the webserver on (default: 8000)", default=8000, type=int)
+    args = parser.parse_args()
+    logger.info(args)
+    webserver_port = args.port
     asyncio.run(main())
